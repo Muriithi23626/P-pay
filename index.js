5cd726b09781ccc9403b34a080e5277f6268430b
Muriithi23626 <113465733+Muriithi23626@users.noreply.github.com>
2022-09-13 22:03:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let amount = document.getElementById(input)
+let request = console.log("10,000")
+let message="hi kelvin"
+
+
+function put(){
+ amount.textContent= message
+ console.log(message)
+}
+
+      
+

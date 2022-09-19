e8c840bc0b4cab506eb04cf6b3e9c42c4dd85b84
Muriithi23626 <113465733+Muriithi23626@users.noreply.github.com>
2022-09-19 22:35:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 
 
-function login() {
-var user = document.getElementById("username").value;
-var pass = document.getElementById("password").value;
-if(user == "lab18" && pass == "lab18") {
-    alert("Logged In");
-    locate="new11.html"
-    return false;
-} else {
-    alert("wrong user/pass");
-    return false;
-    }
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
 }
    
 
